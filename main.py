4e68f980a08d047bf3e7f26e02e254351124f9f0
Ali Haj-Hashemi <alifarhikhtegan@gmail.com>
2021-01-16 02:59:48 +0330

    Faz two complete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pandas._libs.tslibs import Timestamp
 from ui import Ui_MainWindow
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QHeaderView, QTableWidgetItem
@@ -16,6 +17,7 @@ class Ui(Ui_MainWindow):
         self.comboBox.currentIndexChanged.connect(self.faz_one)
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.faz2_btn.clicked.connect(self.faz_two)
 
     def setDataDir(self):
         self.dataDir = str(QFileDialog.getExistingDirectory(
@@ -30,10 +32,6 @@ class Ui(Ui_MainWindow):
             self.comboBox.addItems(self.csvFile)
 
     def faz_one(self):
-        show_data_thread = threading.Thread(target=self.show_data)
-        show_data_thread.start()
-
-    def show_data(self):
 
         if self.tableFlag:
             self.tableWidget.clearContents()
@@ -48,6 +46,47 @@ class Ui(Ui_MainWindow):
         for x in range(df.shape[0]):
             for y, data in enumerate(df.loc[x]):
                 self.tableWidget.setItem(x, y, QTableWidgetItem(str(data)))
+
+    def faz_two(self):
+        faz_two_thread = threading.Thread(target=self.faz_two_thread)
+        faz_two_thread.start()
+
+
+    def faz_two_thread(self):
+        df = pd.read_csv(os.path.join(self.dataDir, 'people.csv'))
+        workerList = df.loc[(df["work"] == "سازمان بنادر") | (df["work"] == "گمرک")]["ssn"]
+        workerList = list(item for item in workerList)
+        df = pd.read_csv(os.path.join(self.dataDir, 'relationships.csv'))
+        workersFamilyList = df.loc[df["from"].isin(workerList)]["to"]
+        workersFamilyList = list(item for item in workersFamilyList)
+        workersFamilyList.extend(workerList)
+        ssnList = list(set(workersFamilyList))
+        del workersFamilyList
+        import datetime
+        date = datetime.datetime.now().date() - datetime.timedelta(days=2*365)
+        date = str(date)
+        date = date.split('-')
+        date = Timestamp(year=int(date[0]), month=int(date[1]), day=int(date[2]))
+        df = pd.read_csv(os.path.join(self.dataDir, 'ownerships.csv'), parse_dates=[3])
+        susList = df.loc[(df["from"].isin(ssnList)) & (df["date"] >= date)]["from"]
+        susList = list(item for item in susList)
+        susList = list(set(susList))
+        df = pd.read_csv(os.path.join(self.dataDir, 'people.csv'))
+        resultData = df.loc[df["ssn"].isin(susList)]
+        temp = resultData.loc[(resultData["work"] != "سازمان بنادر") | (resultData["work"] != "گمرک")]["ssn"]
+        temp = list(item for item in temp)
+        df = pd.read_csv(os.path.join(self.dataDir, 'relationships.csv'))
+        df = df.loc[(df["to"].isin(temp)) & df["from"].isin(workerList)]["from"]
+        df = list(item for item in df)
+        susList = set(susList) - set(temp)
+        df.extend(susList)
+        susList =list(set(df))
+        df = pd.read_csv(os.path.join(self.dataDir, 'people.csv'))
+        resultData = df.loc[df["ssn"].isin(susList)]
+        resultData.to_csv(os.path.join(self.dataDir, 'faz2.csv'), columns=['first_name','last_name','ssn','birthday','city','work'], index=False)
+        self.comboBox.addItem('faz2.csv')
+        self.comboBox.setCurrentText('faz2.csv')
+
 
 
 if __name__ == "__main__":
